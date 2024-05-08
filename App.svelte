e6c8b4ece9481206e470793c29e6d8372467976b
SarahSchrevens <115085748+SarahSchrevens@users.noreply.github.com>
2024-05-08 17:37:12 +0200

    try-out piechart

---
+++ App.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Pie from../Pie.sveltete"
+    let data = [((35/360)*3),((70/360)*3),((130/360)*3)] // sum = 360
+  </script>
+  
+  <Pie data={data} />
---
+++ Pie.svelte
@@ -0,0 +1,28 @@
+<script>
+    import PieSlice from './PieSlice.svelte';
+    export let data = []
+  
+    let slices = []
+    let running_sum = 0
+    $: {
+      data.forEach(function(d) {
+        let new_running_sum = running_sum + d;
+        slices.push([running_sum, new_running_sum])
+        running_sum = new_running_sum;
+      })
+    }
+  </script>
+  
+  <style>
+    svg {
+      background-color: whitesmoke;
+    }
+  </style>
+  
+  <svg width=200 height=200>
+    {#each slices as slice}
+      <PieSlice cx=100 cy=100
+                r=50
+                start_degree={slice[0]} stop_degree={slice[1]} />
+    {/each}
+  </svg>
